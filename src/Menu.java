274790b4a046c1d1ec8d547e887e85864e92efe5
Davi Chaves <dchaves@usc.edu>
2015-01-20 13:50:28 -0800

    First Commit

---
+++ src/Menu.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Menu {
+	public static void main(String [] args){
+		while(true){
+			System.out.println ("Menu: (type 1)");
+			Scanner s = new Scanner ( System.in );
+		}
+	}
+
+}
